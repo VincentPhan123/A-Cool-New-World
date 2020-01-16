a4da171cdfeeb3dae10766c8dd33b709824c7fd5
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-01-15 19:08:50 -0500

    Update groceryStore.java

--- groceryStore.java
+++ groceryStore.java
@@ -1,29 +1,44 @@
+
 import java.util.*;
-public class groceryStory {
+public class groceryStore {
 	public static void main (String[] args) {
-		boolean done = false;
-		String input = "";
 		Scanner reader = new Scanner(System.in);
 		ArrayList<Object> al = new ArrayList <Object> ();
-		System.out.println("GROCERY STORY INVENTORY");
+		System.out.println("GROCERY STORE INVENTORY");
+		menu(al, reader);
+	}
+	
+	static void menu(ArrayList al, Scanner reader) {
+		boolean done = false;
+		String input = "";
 		
 		while(!done) {
-			System.out.println("1) ADD ITEMS\n2) Change Price\n3) Change Quantites\n4) Check Item Status\n5) Check Total Inventory\n6) Help\n8) Quit");
+			System.out.println("1) Add Items\n2) Change Price\n3) Change Quantites\n4) Check Item Status\n5) Check Total Inventory\n6) Item List\n7) Quit");
 			input = reader.next();
 			if(input.equals("1")) {
 				addItems(al , reader);
-				System.out.println(al);
-			}if(input.contentEquals("2")){
+			}else if(input.equals("2")){
 				priceItems(al, reader);
-				System.out.println(al);
+			}else if(input.equals("3")){
+				addQuantity(al, reader);
+			}else if(input.equals("4")){
+				checkItem(al, reader);
+			}else if(input.equals("5")) {
+				inventoryCheck(al,reader);
+			}else if(input.equals("6")){
+				printList(al);
+			}else if(input.equals("7")) {
+				done = true;
 			}
 		}
 	}
 	
-	
 	static void addItems(ArrayList al, Scanner reader) {
-		System.out.println("Enter the item you would like to add:");
-		String input = reader.next();
+		System.out.println("Enter b to go to back the MENU\nEnter item:");
+		String input = reader.next().toUpperCase();
+		if(input.contentEquals("B")) {
+			menu(al,reader);
+		}
 		al.add(input);
 		System.out.println("Enter the price:");
 		Double inputPrice = reader.nextDouble();
@@ -33,21 +48,84 @@ public class groceryStory {
 		al.add(inputAmount);		
 	}
 	
-	static void addQuantity(ArrayList al) {
-		
+	static void addQuantity(ArrayList al, Scanner reader) {
+		System.out.println("Enter b to go to back the MENU\nQuantity Change\nEnter item:\n" + al);
+		String input = reader.next().toUpperCase();
+		if(input.contentEquals("B")) {
+			menu(al,reader);
+		}
+		if(!al.contains(input)) {
+			System.out.println("Sorry that item is not in the system. Enter your item again.");
+			addQuantity(al, reader);
+		}
+		int index = al.indexOf(input);
+		index += 2;
+		System.out.println("Ok what will be " + input + "'s new quantity?\n" + al);
+		int newQuantity = reader.nextInt();
+		al.set(index, newQuantity);
 	}
 	
 	static void priceItems(ArrayList al, Scanner reader) {
-		System.out.println("What price do you want to change for an item?\n" + al);
-		String input = reader.next();
-		int index = al.indexOf(input + 1);
+		System.out.println("Enter b to go to back the MENU\nPrice Change\nEnter Item:\n" + al);
+		String input = reader.next().toUpperCase();
+		if(input.contentEquals("B")) {
+			menu(al,reader);
+		}
+		if(!al.contains(input)) {
+			System.out.println("Sorry that item is not in the system. Enter your item again.");
+			priceItems(al, reader);
+		}
+		int index = al.indexOf(input);
+		index += 1;
 		System.out.println("Ok what will be " + input + "'s new price?");
 		double newPrice = reader.nextDouble();
 		al.set(index , newPrice);
 	}
 	
+	static void checkItem(ArrayList al, Scanner reader) {
+		System.out.println("Enter b to go to back the MENU\nStatus Check\nEnter Item: ");
+		String input = reader.next().toUpperCase();
+		if(input.contentEquals("B")) {
+			System.out.println("Item List\n");
+			menu(al,reader);
+		}
+		if(!al.contains(input)) {
+			System.out.println("Sorry that item is not in the system. Enter your item again.");
+			checkItem(al, reader);
+		}
+		int index1 = al.indexOf(input);
+		index1 += 1;
+		int index2 = al.indexOf(input);
+		index2 += 2;
+		System.out.println(input + " price: $" + al.get(index1) + " quantity: " + al.get(index2));
+	}
 	
+	static void printList(ArrayList al) {
+		System.out.println(al);
+	}
+	
+	static void inventoryCheck(ArrayList al, Scanner reader) {
+		System.out.println("Enter b to go to back the MENU\nInventory Check");
+		int inventory = 0;
+		double totalPrice = 0;
+		
+		if(al.size() == 3) {
+			System.out.println("Total Inventory: " + al.get(2));
+		}else if(al.size() >= 6) {
+			for(int i = 2; i < al.size(); i += 3) {
+				inventory += (int)al.get(i);
+			}
+			System.out.println("Total Inventory: " + inventory);
+		}
+		
+		if(al.size() == 3) {
+			System.out.println("Total Price: " + al.get(1));
+		}else if(al.size() >= 6) {
+			for(int i = 1; i < al.size(); i += 3) {
+				totalPrice += (double)al.get(i);
+			}
+			System.out.println("Total price: $" + totalPrice);
+		}
+		
+	}
 }
-
-
-
