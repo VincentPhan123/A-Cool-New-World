8577d07a53bf5028c83757f689534b48325829c7
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-01-22 19:48:56 -0500

    Update stringMethods.java

--- stringMethods.java
+++ stringMethods.java
@@ -1,50 +1,28 @@
-import java.util.Random.*;
 import java.util.*;
 
-public class Main {
+public class stringgMethods {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		boolean done = false;
-		System.out.println("Welcom to my string  method program\n -----------------");
-		
-		while(!done){
-			
-			System.out.println("Choose a method you like to use:\n A) isPalindrom\n B) findString\n C) replaceString\n D) stringCount\n E) shuffelString\n F) quit");
-			String userInput = reader.next().toUpperCase();
-
-			if(userInput.equals("A")) {
-				System.out.println("Enter your word: ");
-				String userWord = reader.next().toLowerCase();
-				isPalindrom(userWord);
-			}else if (userInput.equals("B")) {
-				System.out.println("Enter a passage:");
-				String userPassage = reader.nextLine().toLowerCase();
-				System.out.println("Enter your target word: ");
-				String userTarget = reader.next().toLowerCase();
-				findString(userPassage, userTarget);
-			}else if (userInput.equals("C")) {
-				System.out.println("Enter your passage: ");
-				String userPassage = reader.nextLine().toLowerCase();
-				System.out.println("Enter your target word: ");
-				String userTarget = reader.next().toLowerCase();
-				System.out.println("Enter your new word: ");
-				String userReplace = reader.next();
-				replaceString(userPassage, userTarget, userReplace);
-			}else if(userInput.equals("D")) {
-				System.out.println("Enter a passage:");
-				String userPassage = reader.nextLine().toLowerCase();
-				System.out.println("Enter your target word: ");
-				String userTarget = reader.next().toLowerCase();
-				stringCount(userPassage, userTarget);
-			}else if(userInput.equals("E")) {
-				System.out.println("Enter a word you like to shuffel: ");
-				String userWord = reader.next().toLowerCase();
-				shuffelString(userWord);
-			}
-			else{
-				done = true;
-			}
-		}
+		System.out.println("Enter a passage:");
+		String userPassage = reader.nextLine();
+		String [] arrayPassage = userPassage.split(" ");
+		System.out.println("Enter your target word:");
+		String userTarget = reader.next();
+		findString(arrayPassage, userTarget);
+		System.out.println("Enter the word you would like to replace:");
+		String userTarget2 = reader.next();
+		System.out.println("Enter you replacement:");
+		String userReplace = reader.next();
+		replaceString(arrayPassage, userTarget2, userReplace);
+		System.out.println("Enter the word to see how many times it occurs in the passage:");
+		String userTarget3 = reader.next();
+		stringCount(arrayPassage, userTarget3);
+		System.out.println("Enter a word to see if it is a palindrom:");
+		String userWord = reader.next();
+		isPalindrom(userWord);
+		System.out.println("Enter a word you would like to shuffle:");
+		String userWord2 = reader.next();
+		shuffleString(userWord2);
 		
 	} 
 	
@@ -60,44 +38,49 @@ public class Main {
 		}
 	}
 	
-	static void findString(String userPassage, String userTarget) {
-		int index = userPassage.indexOf(userTarget);
-		while(index >= 0){
-			System.out.print("Index(s): " + index);
-			index = userPassage.indexOf(userTarget, index + userTarget.length());  
+	static void findString(String [] arrayPassage, String userTarget) {
+		String index = "";
+		for(int i = 0; i < arrayPassage.length; i++) {
+			if(arrayPassage[i].equals(userTarget)) {
+				index += i + " ";
+				}
 		}
+		System.out.println("The index(s) of " + userTarget + ": " + index);
 	}				
 	
-	static void replaceString(String userPassage, String userTarget, String userReplace) {
-		int index = userPassage.indexOf(userTarget);
-		List<String> passageArray = new ArrayList<String>();
-		passageArray.add(userPassage);
-
-		passageArray.set(index, userReplace);
-
-		System.out.println(passageArray);	
-	}
-	
-	static void stringCount(String userPassage, String userTarget) {
-		int index = userPassage.indexOf(userTarget);
-		int numTarget = 0;
-		while(index >= 0){
-			numTarget++;
-			System.out.println(userTarget + " occurs: " + numTarget);
-			index = userPassage.indexOf(userTarget, index + userTarget.length());  	
+	static void replaceString(String [] arrayPassage, String userTarget2, String userReplace) {
+		String passage = "";
+		for(int i = 0; i < arrayPassage.length; i++) {
+			if(arrayPassage[i].equals(userTarget2)) {
+				arrayPassage[i] = userReplace;
+			}
 		}
+		for(int i = 0; i < arrayPassage.length; i++) {
+			passage += arrayPassage[i] + " ";
+		}
+		System.out.println(passage);
 	}
 	
-	static void shuffelString(String userWord) {
+	static void stringCount(String [] arrayPassage, String userTarget3) {
+		int count = 0;
+		for(int i = 0; i < arrayPassage.length; i++) {
+			if(arrayPassage[i].equals(userTarget3)) {
+				count++;
+			}
+		}
+		System.out.println(userTarget3 + " occurs " + count + " times.");
+	}
+	
+	static void shuffleString(String userWord2) {
 		Random random = new Random();
-		int wordLength = userWord.length();
-		int randomChar = random.nextInt(wordLength);
-		char[] charArray =  userWord.toCharArray();
+		int randomChar = random.nextInt(userWord2.length());
+		char[] charArray =  userWord2.toCharArray();
+		char temp;
 
-		for(int i = 0; i < wordLength; i ++){
-				char temp = charArray[i];
-        charArray[i] = charArray[randomChar];
-        charArray[randomChar] = temp;
+		for(int i = 0; i < userWord2.length(); i ++){
+			temp = charArray[i];
+			charArray[i] = charArray[randomChar];
+			charArray[randomChar] = temp;
 		}
 
 		System.out.println(charArray);
