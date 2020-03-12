fe3b87ac9827d3a2609eaffce2e179a99538f3da
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-03-11 21:06:50 -0400

    Update vendingMachine.java

--- vendingMachine.java
+++ vendingMachine.java
@@ -1,12 +1,42 @@
 import java.util.*;
 public class VendingMachine{
-    private Item [][] vendingMachine = Item(n,d);
+    private VendingMachine [][] vendingMachine = new VendingMachine[5][3];
     private double money;
 
-    public VendingMachine(int [][] a, double m){
+    public VendingMachine(VendingMachine [][] a, double m){
         this.vendingMachine = a;
         this.money = m;
-        Random rand = new Random();
-        m = rand.nextInt(100);
+
+        a[0][0] = new Item ("Ratatouille", 2);
+        a[0][1] = new Item ("Pizza Planet's Pizza", 1.50);
+        a[0][2] = new Item ("B.D's Bath Water", 2);
+        a[1][0] = new Item ("Jolly Ranchers", 1);
+        a[1][1] = new Item ("Gummy Bears", 1.25);
+        a[1][2] = new Item ("Ramen Ichiraku's Ramen", 2);
+        a[2][0] = new Item ("Pho In A Cup", .25);
+        a[2][1] = new Item ("Greg's Egg Rolls", .15);
+        a[2][2] = new Item ("Usinger's Bratwurts", .50);
+        a[3][0] = new Item ("Water Sheep's Lambchop", 2);
+        a[3][1] = new Item ("Amy's Angel Cake", 2);
+        a[3][2] = new Item ("Tacos On A Stick", 1.50);
+        a[4][0] = new Item ("Betty's Brownies", .50);
+        a[4][1] = new Item ("Benny's Burritos", .25);
+        a[4][2] = new Item ("Totoro's Tuna", 1.25);
+         
+        for(int rows = 0; rows < a.length; rows++){
+            for(int cols = 0; cols < a[0].length; col++){
+                System.out.printf("%-10s", a[rows][cols]);
+            }
+        }
     }
+
+    /*
+    public static void getVendingMachine(){
+        for(int rows = 0; rows < a.length; rows++){
+            for(int cols = 0; cols < a[0].length; col++){
+                System.out.printf("%-10s", a[rows][cols]);
+            }
+        }
+    }
+    */
 }
