644ec1ced20af1d027d0a2c8b2e8b458e0da2e29
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-03-11 21:07:13 -0400

    Update item.java

--- item.java
+++ item.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Item{
     private String items;
     private double price; 
-    private int  quantity; 
+    private int quantity; 
 
     public Item(String i, double p){
-        this.items = n;
+        this.items = i;
         this.price = p;
         this.quantity = 10;
     }
