117a913e705c7190377fe1c08eff35cd297e7840
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-03-12 23:47:56 -0400

    Update and rename item.java to Items.java

---
+++ Items.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Items{
+    final String items;
+    final double price; 
+    int quantity; 
+
+    public Items(String i, double p){
+        this.items = i;
+        this.price = p;
+        this.quantity = 10;
+    }
+}
