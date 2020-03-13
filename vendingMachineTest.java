ff343b55fd524d19a5cecaca3cc25276e276ba18
VincentPhan123 <54594154+VincentPhan123@users.noreply.github.com>
2020-03-12 23:47:16 -0400

    Update vendingMachineTest.java

--- vendingMachineTest.java
+++ vendingMachineTest.java
@@ -1,5 +1,11 @@
-public class vendingMachingTest{
+public class vendingMachineTest {
     public static void main(String[] args) {
-        VendingMachine arr = new VendingMachine();
-    }
+         Items [][] ar = new Items [5][3];
+         boolean done = false;
+
+         while(!done){
+            vendingMachine arr = new vendingMachine(ar);
+            done = true;
+         }
+     }
 }
